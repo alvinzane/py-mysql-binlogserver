3ad99dfa13dbeeb40ea475fe58377bdd851fb702
alvin <awary@qq.com>
2019-10-10 22:04:21 +0800

    Add tutorial code

--- py_mysql_binlogserver/_tutorial/learn-socket3-server-mulit-thread.py
+++ py_mysql_binlogserver/_tutorial/learn-socket3-server-mulit-thread.py
@@ -6,16 +6,19 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         """
-        计算器，返回表达式的值
+        网络计算器，返回表达式的值
         """
         while True:
             try:
+                # 接收表达式数据
                 data = str(self.request.recv(1024), 'ascii').strip()
                 if "q" in data:
                     self.finish()
                     break
+                # 计算结果
                 response = bytes("{} = {}\r\n".format(data, eval(data)), 'ascii')
                 print(response.decode("ascii").strip())
+                # 返回结果
                 self.request.sendall(response)
             except:
                 self.request.sendall(bytes("\n", 'ascii'))
